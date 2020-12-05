ad8aef6d7ce1f9b71f0de588809014027e542e1f
lucas DELOLME <lucas.delolme@orange.fr>
2020-12-05 15:19:29 +0100

    [ADD] every random command in their own categoruy

---
+++ Commands/RandomCommands.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Valhallapp.Modules
+{
+    public class RandomCommands : ModuleBase<SocketCommandContext>
+    {
+        private readonly SimpleCommands command = new SimpleCommands();
+
+        [Command("furry")]
+        public async Task FurryCommand() => await command.RandomCommand("furry", 1, 100);
+
+        [Command("furry")]
+        public async Task FurryCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 1, 100);
+
+        [Command("femboy")]
+        public async Task FemboyCommand() => await command.RandomCommand("femboy", 0, -1);
+
+        [Command("femboy")]
+        public async Task FemboyCommand([Remainder] string param) => await command.RandomCommand(param, "femboy", 0, -1);
+
+        [Command("gay")]
+        public async Task GayCommand() => await command.RandomCommand("gay", 1, -1);
+
+        [Command("gay")]
+        public async Task GayCommand([Remainder] string param) => await command.RandomCommand(param, "gay", 1, -1);
+
+        [Command("realfurry")]
+        public async Task RealFurryCommand() => await command.RandomCommand("furry", 2, -1);
+
+        [Command("realfurry")]
+        public async Task RealFurryCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 2, -1);
+
+        [Command("slut")]
+        public async Task SlutCommand() => await command.RandomCommand("furry", 2, -1);
+
+        [Command("slut")]
+        public async Task SlutCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 2, -1);
+    }
+}
