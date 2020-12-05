ae33e569c50a6171bdc8b5b6de6e5bf3590a863e
lucas DELOLME <lucas.delolme@orange.fr>
2020-12-05 18:40:33 +0100

    [ADD] update to the update

--- Commands/RandomCommands.cs
+++ Commands/RandomCommands.cs
@@ -11,33 +11,33 @@ namespace Valhallapp.Modules
         private readonly SimpleCommands command = new SimpleCommands();
 
         [Command("furry")]
-        public async Task FurryCommand()=>await command.RandomCommand("furry", 1, 100);
+        public async Task FurryCommand()=>await command.RandomCommand(Context,"furry", 1, 100);
 
         [Command("furry")]
-        public async Task FurryCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 1, 100);
+        public async Task FurryCommand([Remainder] string param) => await command.RandomCommand(Context, param, "furry", 1, 100);
 
         [Command("femboy")]
-        public async Task FemboyCommand() => await command.RandomCommand("femboy", 0, -1);
+        public async Task FemboyCommand() => await command.RandomCommand(Context, "femboy", 0, -1);
 
         [Command("femboy")]
-        public async Task FemboyCommand([Remainder] string param) => await command.RandomCommand(param, "femboy", 0, -1);
+        public async Task FemboyCommand([Remainder] string param) => await command.RandomCommand(Context, param, "femboy", 0, -1);
 
         [Command("gay")]
-        public async Task GayCommand() => await command.RandomCommand("gay", 1, -1);
+        public async Task GayCommand() => await command.RandomCommand(Context, "gay", 1, -1);
 
         [Command("gay")]
-        public async Task GayCommand([Remainder] string param) => await command.RandomCommand(param, "gay", 1, -1);
+        public async Task GayCommand([Remainder] string param) => await command.RandomCommand(Context, param, "gay", 1, -1);
 
         [Command("realfurry")]
-        public async Task RealFurryCommand() => await command.RandomCommand("furry", 2, -1);
+        public async Task RealFurryCommand() => await command.RandomCommand(Context, "furry", 2, -1);
 
         [Command("realfurry")]
-        public async Task RealFurryCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 2, -1);
+        public async Task RealFurryCommand([Remainder] string param) => await command.RandomCommand(Context, param, "furry", 2, -1);
 
         [Command("slut")]
-        public async Task SlutCommand() => await command.RandomCommand("furry", 3, -1);
+        public async Task SlutCommand() => await command.RandomCommand(Context, "furry", 3, -1);
 
         [Command("slut")]
-        public async Task SlutCommand([Remainder] string param) => await command.RandomCommand(param, "furry", 3, -1);
+        public async Task SlutCommand([Remainder] string param) => await command.RandomCommand(Context, param, "furry", 3, -1);
     }
 }
--- Commands/SimpleCommands.cs
+++ Commands/SimpleCommands.cs
@@ -45,25 +45,23 @@ namespace Valhallapp.Modules
         }
 
         // RANDOM COMMAND BASIC FUNCTIONS
-        public async Task RandomCommand(string name, ulong randomModifier, int isRigged)
+        public async Task RandomCommand(SocketCommandContext context, string name, ulong randomModifier, int isRigged)
         {
-            Console.WriteLine("No param");
-            Random rnd = new Random((int)(Convert.ToUInt64(Context.User.Id) + randomModifier % 10000000));
-            Console.WriteLine("No param2");
+            DisplayCommandLine(name, context.Message.Author.Username, context.Channel.Name);
+            Random rnd = new Random((int)(Convert.ToUInt64(context.User.Id) + randomModifier % 10000000));
             if (isRigged != -1)
                 await ReplyAsync(embed:
-                    PostEmbedPercent(Context.User.Username, $"<@{Context.User.Id}>", Context.User.GetAvatarUrl(), rnd.Next(101), name));
+                    PostEmbedPercent(context.User.Username, $"<@{context.User.Id}>", context.User.GetAvatarUrl(), rnd.Next(101), name));
             else
                 await ReplyAsync(embed:
-                    PostEmbedPercent(Context.User.Username, $"<@{Context.User.Id}>", Context.User.GetAvatarUrl(), isRigged, name));
+                    PostEmbedPercent(context.User.Username, $"<@{context.User.Id}>", context.User.GetAvatarUrl(), isRigged, name));
         }
 
-        public async Task RandomCommand([Remainder] string param, string name, ulong randomModifier, int isRigged)
+        public async Task RandomCommand(SocketCommandContext context, [Remainder] string param, string name, ulong randomModifier, int isRigged)
         {
-            Console.WriteLine($"params");
+            DisplayCommandLine($"{name} with params", context.Message.Author.Username, context.Channel.Name);
             Random rnd;
             string userID = param;
-            Console.WriteLine($"{name} with params");
             // userping function
             if (param.Contains("<@"))
             {
@@ -71,12 +69,12 @@ namespace Valhallapp.Modules
                 userID = userID.Substring(2, userID.Length - 2);
                 if (userID[0] == '!') userID = userID.Substring(1, userID.Length - 1);
                 rnd = new Random((int)(Convert.ToUInt64(userID) + randomModifier % 10000000));
-                if (isRigged!=-1)
+                if (isRigged != -1)
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), rnd.Next(101), name));
+                        PostEmbedPercent(context.User.Username, $"<@{userID}>", context.User.GetAvatarUrl(), rnd.Next(101), name));
                 else
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), isRigged, name));
+                        PostEmbedPercent(context.User.Username, $"<@{userID}>", context.User.GetAvatarUrl(), isRigged, name));
 
             }
             // id function
@@ -84,13 +82,13 @@ namespace Valhallapp.Modules
             {
                 rnd = new Random((int)(Convert.ToUInt64(userID) + randomModifier % 10000000));
                 if (Convert.ToUInt64(userID) == 156997866605248512) await ReplyAsync(embed:
-                    PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), 101, name));
+                    PostEmbedPercent(context.User.Username, $"<@{userID}>", context.User.GetAvatarUrl(), 101, name));
                 if (isRigged != -1)
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), rnd.Next(101), name));
+                        PostEmbedPercent(context.User.Username, $"<@{userID}>", context.User.GetAvatarUrl(), rnd.Next(101), name));
                 else
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), isRigged, name));
+                        PostEmbedPercent(context.User.Username, $"<@{userID}>", context.User.GetAvatarUrl(), isRigged, name));
 
             }
             // TODO: username function
@@ -100,10 +98,10 @@ namespace Valhallapp.Modules
                 rnd = new Random();
                 if (isRigged != -1)
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"{param}", Context.User.GetAvatarUrl(), rnd.Next(101), name));
+                        PostEmbedPercent(context.User.Username, $"{param}", context.User.GetAvatarUrl(), rnd.Next(101), name));
                 else
                     await ReplyAsync(embed:
-                        PostEmbedPercent(Context.User.Username, $"{param}", Context.User.GetAvatarUrl(), isRigged, name));
+                        PostEmbedPercent(context.User.Username, $"{param}", context.User.GetAvatarUrl(), isRigged, name));
 
             }
         }
