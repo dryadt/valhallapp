585f7b2fad05bdcaece48fba27b143fdfccb3a71
lucas DELOLME <lucas.delolme@orange.fr>
2020-12-05 12:11:45 +0100

    [FIX] channel are now used directly

--- Commands/Gay.cs
+++ Commands/Gay.cs
@@ -34,20 +34,18 @@ namespace Valhallapp.Modules
                 userID = userID.Substring(2, userID.Length - 2);
                 if (userID[0] == '!') userID = userID.Substring(1, userID.Length - 1);
                 rnd = new Random((int)(Convert.ToUInt64(userID) + 1 % 10000000));
-                if (Convert.ToUInt64(userID)== 156997866605248512)
-                    await ReplyAsync(embed:
-                            PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), 101, "gay"));
-                await ReplyAsync(embed:
+                if (Convert.ToUInt64(userID)== 156997866605248512) await ReplyAsync(embed:
+                    PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), 101, "gay"));
+                else await ReplyAsync(embed:
                     PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), rnd.Next(101), "gay"));
             }
             // id function
             else if (IsDigitsOnly(userID))
             {
                 rnd = new Random((int)(Convert.ToUInt64(userID) + 1 % 10000000));
-                if (Convert.ToUInt64(userID) == 156997866605248512)
-                    await ReplyAsync(embed:
-                            PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), 101, "gay"));
-                await ReplyAsync(embed:
+                if (Convert.ToUInt64(userID) == 156997866605248512) await ReplyAsync(embed:
+                    PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), 101, "gay"));
+                else await ReplyAsync(embed:
                     PostEmbedPercent(Context.User.Username, $"<@{userID}>", Context.User.GetAvatarUrl(), rnd.Next(101), "gay"));
             }
             // TODO: username function
