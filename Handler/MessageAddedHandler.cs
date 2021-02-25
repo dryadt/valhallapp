4c144f5f08a03fad33af2161026fecdba6ad0362
lucas DELOLME <lucas.delolme@orange.fr>
2021-02-25 14:19:45 +0100

    [add] message content in url post

--- Handler/MessageAddedHandler.cs
+++ Handler/MessageAddedHandler.cs
@@ -83,7 +83,7 @@ namespace valhallappweb
                 if (isEmbedable)
                     await galleryTalkChannel.SendMessageAsync(embed:
                         PostEmbedImage(message.Author.Username, message.Author.Id, Regex.Replace(message.Content, @"http[^\s]+", ""), message.Author.GetAvatarUrl(), url, message.Id));
-                else await MessageChannel(_client, $"{message.Author.Username} posted: {url}", galleryTalkId);
+                else await MessageChannel(_client, $"{message.Author.Username} posted:\n{Regex.Replace(message.Content, @"http[^\s]+", "")} {url}", galleryTalkId);
             };
         }
     }
