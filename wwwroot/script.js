3b0e371467d43e124458c48974457156d7cf66bc
lucas DELOLME <lucas.delolme@orange.fr>
2021-05-21 16:43:35 +0200

    [add] remove json file access to have the website working

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -17,9 +17,46 @@ const maxExplIndex = 19;
 // artSelection
 
 var selectedArt = 0;
-const fs = require('fs');
-let imageUrl = JSON.parse(fs.readFileSync("imageUrl.json"));
-let artistList = JSON.parse(fs.readFileSync("artistList.json"));
+let imageUrl = [
+    { credit: 1, url: 'https://media.discordapp.net/attachments/467635425553547307/720005811602063452/Starless.png?width=225&height=300' },
+    { credit: 2, url: 'https://cdn.discordapp.com/attachments/482894390570909706/742330962423185469/starless-1.png' },
+    { credit: 3, url: 'https://media.discordapp.net/attachments/601614220575899648/733139015825489940/calebhug.png' },
+    { credit: 3, url: 'https://media.discordapp.net/attachments/601614220575899648/733139013371953240/calebboop.png' },
+    { credit: 4, url: 'https://cdn.discordapp.com/attachments/482894390570909706/751013108142702652/unknown.png' },
+    { credit: 3, url: 'https://cdn.discordapp.com/attachments/577586063967518826/769582547604340747/unknown-71.png' },
+    { credit: 3, url: 'https://cdn.discordapp.com/attachments/598604322623848458/689494198591946874/dryadnuzzles.png' },
+    { credit: 5, url: 'https://cdn.discordapp.com/attachments/482894390570909706/786159395959734282/image1.png' },
+    { credit: 7, url: 'https://cdn.discordapp.com/attachments/577586063967518826/787819431463747645/starless_black.png' },
+    { credit: 2, url: 'https://cdn.discordapp.com/attachments/718478016241467453/791583295372394556/bgtest.png' },
+    { credit: 6, url: 'https://cdn.discordapp.com/attachments/482894390570909706/791966004607582228/Cukier49_-_Antares_and_Starless.png' },
+    { credit: 8, url: 'https://cdn.discordapp.com/attachments/482894390570909706/800466954921836624/StarlessMuffinv2.png' },
+    { credit: 9, url: 'https://cdn.discordapp.com/attachments/482894390570909706/799602382459633664/nostarsatall.png' },
+    { credit: 10, url: 'https://cdn.discordapp.com/attachments/577586063967518826/807705446911180830/le_cert.png' },
+    { credit: 7, url: 'https://cdn.discordapp.com/attachments/690831388408021043/813448241789534248/Skye_Starless.png' },
+    { credit: 6, url: 'https://cdn.discordapp.com/attachments/482894390570909706/810458961244454942/Stellar_art_Cukier.jpg' },
+    { credit: 7, url: 'https://cdn.discordapp.com/attachments/577586063967518826/816803046847676487/full_res_normal_heart.png' },
+    { credit: 7, url: 'https://cdn.discordapp.com/attachments/742482228331675708/821137002598367272/emote2_heart_transparent2.png' },
+    { credit: 7, url: 'https://cdn.discordapp.com/attachments/742482228331675708/823834471848738846/Starless_HS.png' },
+    { credit: 3, url: 'https://cdn.discordapp.com/attachments/482894390570909706/823700988569452584/nuzzles1.png' },
+    { credit: 11, url: 'https://cdn.discordapp.com/attachments/742482228331675708/823833604295360532/mere_comm_png_-1.png' },
+    { credit: 9, url: 'https://cdn.discordapp.com/attachments/482894390570909706/841260596568522763/Screenshot_2.png' },
+    //{ credit: 0, url: 'url' },
+]
+
+let artistList = [
+    "Art by: <a href='https://twitter.com/'></a>",
+    "Art by: <a href='https://twitter.com/RockAddictGlum'>Glum</a>",
+    "Art by: <a href='https://twitter.com/Spaztiqe'>Spaz</a>",
+    "Art by: Meg",
+    "Art by: Mewdusa",
+    "Art by: Ollie",
+    "Art by: <a href='https://twitter.com/Cukier49'>Cukier</a>",
+    "Art by: <a href='https://twitter.com/JumpMere'>Mere</a>",
+    "Art by: InfernalBraxia",
+    "Art by: Blight",
+    "Art by: Aurora",
+    "Art by: <a href='https://twitter.com/Kadus_UwU/media'>Kadus</a>",
+]
 
 // function
 
